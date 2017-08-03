9d3cc1459217a2964868c01f9307a90d293e9eb2
Lopes, Andre <andre.lopes@exxonmobil.com>
2017-08-03 15:04:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+#Study for SASS
+
+Project to study SASS
+
+
+```
+git config --global http.sslVerify false
+
+git config --global http.proxy http://:@HOEPRX80.na.xom.com:8080
+
+git clone https://alopes2@gitserver.xtonet.com/alopes2/sass_study.git
+
+npm install
+```
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const util = require('gulp-util');
+const sass = require('gulp-sass');
+//const plugins = require('gulp-load-plugins')({ lazy: true });
+
+gulp.task('sass', () => {
+    log('Compiling sass');
+    return gulp
+            .src('src/assets/scss/**/*.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('src/assets/css'));
+    }
+);
+
+gulp.task('watch', () => {
+    gulp.watch('src/assets/scss/**/*.scss', ['sass']);
+});
+
+function log(msg) {
+  if (typeof (msg) === 'object') {
+    for (var item in msg) {
+      if (msg.hasOwnProperty(item)) {
+        util.log(util.colors.green(msg[item]));
+      }
+    }
+  } else {
+    util.log(util.colors.green(msg));
+  }
+}
