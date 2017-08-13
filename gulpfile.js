ca67f3cd214756c40ae107ff39b98fb322a7ba8c
Lopes, Andre <andrevitorlopes@gmail.com>
2017-08-13 18:55:50 -0300

    finished sass study

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const util = require('gulp-util');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+
 //const plugins = require('gulp-load-plugins')({ lazy: true });
 
 gulp.task('sass', () => {
@@ -12,11 +14,31 @@ gulp.task('sass', () => {
     }
 );
 
-gulp.task('watch', () => {
+gulp.task('watch',['sass'], () => {
+    log('Watching files');
+    startBrowserSync();
     gulp.watch('src/assets/scss/**/*.scss', ['sass']);
 });
 
-function log(msg) {
+
+const startBrowserSync = () => {
+  log('Starting browser sync');
+  if(browserSync.active)
+    return;
+
+  browserSync.init({
+    server: {
+      baseDir: './src',
+      index: 'simple-menu.html'
+    },
+    files: ['src/**/*.html','src/**/*.js','src/**/*.css'],
+    logPrefix: 'SASS-study',
+    browser: 'Chrome',
+    port: 3000
+  })
+}
+
+const log = msg => {
   if (typeof (msg) === 'object') {
     for (var item in msg) {
       if (msg.hasOwnProperty(item)) {
